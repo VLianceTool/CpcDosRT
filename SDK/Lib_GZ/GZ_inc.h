50075b3de10f97fae4a145f536ad0e25822c4bec
Maeiky <maeiky@gmail.com>
2019-06-29 15:07:24 -0400

    Fix

---
+++ SDK/Lib_GZ/GZ_inc.h
@@ -0,0 +1,88 @@
+
+
+//Common include//
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "Base/GzTypes.h"
+	
+/*
+//////TEMP DEBUG MODE
+#define GZ_tDebug
+
+
+
+
+#ifndef tHDef_GZ
+#define tHDef_GZ
+
+	#include "Lib_GZ/Base/Class.h"
+	
+	#include "Lib_GZ/Base/SmartPtr/gzSp.h"
+	#include "Lib_GZ/Base/SmartPtr/gzWp.h"
+
+	#ifndef GZE_IMPORT
+		#define GZE_IMPORT  __declspec(dllimport)
+	#endif
+
+	
+	#include "Base/GzTypes.h"
+
+	
+#ifndef tHDef_GZ_Minimal_Lib
+#define tHDef_GZ_Minimal_Lib
+	//// Minimal required libs ////
+	
+	#ifndef GZ_STD_limits
+	#define GZ_STD_limits
+		#include <limits.h> //printf
+	#endif
+
+	
+
+	#ifndef GZ_STD_Lib
+	#define GZ_STD_Lib
+		#include <stdlib.h> //printf
+	#endif
+	
+	#ifndef GZ_STD_string
+	#define GZ_STD_string
+
+		#include <string.h>
+	#endif
+		#ifndef GZ_STD_IO
+		#define GZ_STD_IO
+		#include <stdio.h> //printf
+	#endif
+	
+
+	///////////////////////////////
+#endif
+
+#include "Lib_GZ/Debug/Debugging.h"
+
+#include "Lib_GZ/Base/Container/Containers.h" 
+#include "Lib_GZ/Base/GzOverplace.h"
+
+#include "Lib_GZ/Base/Macro.h"
+#include "Lib_GZ/Lib_GZ.h"
+
+
+#include "Lib_GZ/Base/GzBase.h"
+#include "Lib_GZ/Base/Thread/Thread.h"
+#include "Lib_GZ/Base/Class.h"
+#include "Lib_GZ/Base/Thread/StThread.h"
+		
+
+#include "Lib_GZ/Base/Thread/ThreadExt.h"
+
+
+#include "Lib_GZ/Lib.h"
+#include "Lib_GZ/Base/Thread/ThreadMsg.h"
+	
+#include "Lib_GZ/Base/Easing/gzEase.h"
+	
+#endif
+*/
