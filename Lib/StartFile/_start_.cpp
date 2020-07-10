ea1bf7d5f6b43a8c106d99137de95e52d15893b1
Maeiky <maeiky@gmail.com>
2020-07-10 13:32:43 -0400

    Fix Start File

--- Lib/StartFile/_start_.cpp
+++ Lib/StartFile/_start_.cpp
@@ -1,9 +1,14 @@
 #include <stdio.h>
+
+extern "C" int Main();
+
 //__attribute__ ((visibility ("visible"))) 
-extern "C" int _start_(int argc, char **argv)
+//extern "C" int _start_(int argc, char **argv)
+extern "C" int _start_()
 {
-	printf("\n--_start_-- %s\n", argv);
-	return 0;
+	//printf("\n--_start_-- %s\n", argv);
+	printf("\n--_start_-- \n");
+	return Main();
 }
-	
-	
+
+
