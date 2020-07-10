213b2da90c7cf4e99dbb5b054db80f41897199b6
Maeiky <maeiky@gmail.com>
2020-07-10 13:33:36 -0400

    Fix

---
+++ Lib/StartFile/_start_.d
@@ -0,0 +1,17 @@
+StartFile/_start_.o: StartFile\_start_.cpp \
+  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\CpcDosRT\CpcDosRT-0.5\SDK\stdio.h \
+  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\clang_inc\..\i686-w64-mingw32\include\stdio.h \
+  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\crtdefs.h \
+  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\_mingw.h \
+  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\_mingw_mac.h \
+  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\_mingw_secapi.h \
+  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\clang_inc\vadefs.h \
+  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\vadefs.h \
+  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\sdks\_mingw_directx.h \
+  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\sdks\_mingw_ddk.h \
+  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\_mingw_print_push.h \
+  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\_mingw_off_t.h \
+  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\swprintf.inl \
+  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\sec_api\stdio_s.h \
+  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\_mingw_print_pop.h
+Cwc:VLianceTool/CpcDosRT Default clang++ (2019-03-18 14:14:22) >>  -O2 -target i686-pc-mingw32 -I"E:/Data/setting/Test/_Cwc/Toolchain/VLianceTool/LibRT/LibRT-8.0.2/clang_inc/" -I"E:/Data/setting/Test/_Cwc/Toolchain/VLianceTool/LibRT/LibRT-8.0.2/pthreads/" -I"E:/Data/setting/Test/_Cwc/Toolchain/VLianceTool/LibRT/LibRT-8.0.2/lib/gcc/i686-w64-mingw32/8.1.0/include/" -I"E:/Data/setting/Test/_Cwc/Toolchain/VLianceTool/CpcDosRT/CpcDosRT-0.5/Lib/" -fno-rtti -fno-exceptions -m32 -fcolor-diagnostics -fansi-escape-codes -Wno-pragma-pack -m32 -Wglobal-constructors -Wexit-time-destructors -Werror=return-type -Wno-return-type-c-linkage -MD -DD_Render_100_Frames -Wglobal-constructors -Wexit-time-destructors -Werror=global-constructors -Wexit-time-destructors -DD_Platform_Windows -DGZ_D_Monothread -I"E:/Data/setting/Test/_Cwc/Toolchain/VLianceTool/CpcDosRT/CpcDosRT-0.5/SDK/" -DDsToolchain="VLianceTool_CpcDosRT" -DDsPlatform="CpcDos" -DD_Platform_CpcDos -DDsPlatform_File="Default" -DD_Release -DDsOpt="O2"
---
+++ Lib/src/TestStart.cpp
@@ -0,0 +1,241 @@
+// hd.cpp
+
+// hex-dump app
+
+// 12/07/00 (mv)
+// 05/18/02 (mv)  double-space option
+// 07/04/02 (mv)  .NET-ize, general updating
+
+#include <stdio.h>
+
+/*
+#define _CRT_SECURE_NO_DEPRECATE
+#define _CRT_NONSTDC_NO_DEPRECATE
+
+typedef unsigned char BYTE;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+
+#define UNPRINTABLE	_T('.')
+
+FILE *fDump;
+FILE *out;
+int dblSpace;
+
+// translates a character if it is unprintable
+TCHAR GetChar(TCHAR _char)
+{
+	if (_char < 32)
+		return UNPRINTABLE;
+
+	return _char;
+}
+
+// opens the file to dump
+void OpenFile(TCHAR *filename)
+{
+	fDump = _tfopen(filename, _T("rb"));
+	if (!fDump)
+	{
+		_tprintf(_T("Error opening \"%s\"\n"), filename);
+		exit(1);
+	}
+}
+
+// dumps the file
+void DumpFile(TCHAR *filename)
+{
+	// nSpaces solves the problem when a line is incomplete.
+	// The number of spaces between the start of the hex data
+	// and the ASCII text is stored here - if the file does
+	// not end on a 16-bit boundary, the number here will be
+	// the number of spaces to add so the ASCII column will
+	// be aligned properly.
+
+	BYTE fChar;
+	int i=0;
+	long bytesRead=0;
+	TCHAR line[17];
+	int nSpaces=3*16+2;			// spaces from beg. of data
+	memset(&line, 0, sizeof(line));
+
+	// print the filename
+	_tprintf(_T("%s\n\n"), filename);
+
+	if (dblSpace)
+		_tprintf(_T("\n\n"));
+
+	for (;;)
+	{
+		// print hex address
+		_tprintf(_T("%08X  "), i);
+		
+		// print first 8 bytes
+		for (int j = 0; j < 0x08; j++)
+		{
+			fChar = (BYTE)fgetc(fDump);
+			if (feof(fDump))
+				break;
+
+			_tprintf(_T("%02X "), fChar);
+
+			// add to the ASCII text
+			line[bytesRead++] = GetChar(fChar);
+
+			// this took three characters
+			nSpaces -= 3;
+		}
+
+		// print last 8 bytes - change in the "xx " to " xx" provides
+		// the double space in between the first 8 and the last 8 bytes.
+		for (int j = 0x08; j < 0x10; j++)
+		{
+			fChar = (BYTE)fgetc(fDump);
+			if (feof(fDump))
+				break;
+
+			_tprintf(_T(" %02X"), (unsigned char)fChar);
+
+			// add to the ASCII text
+			line[bytesRead++] = GetChar(fChar);
+
+			// this took three characters
+			nSpaces -= 3;
+		}
+
+		// fill in any leftover spaces.
+		for (int j = 0; j <= nSpaces; j++)
+		{
+			_tprintf(_T(" "));
+		}
+
+		// print ASCII text
+		_tprintf(_T("%s"), line);
+
+		// quit if the file is done
+		if (feof(fDump))
+			break;
+
+		// new line
+		_tprintf(_T("\n"));
+
+		if (dblSpace)
+			_tprintf(_T("\n"));
+
+		// reset everything
+		bytesRead=0;
+		memset(&line, 0, sizeof(line));
+		i += 16;
+		nSpaces = 3*16+2;
+	}
+}
+
+// closes everything
+void CloseFile()
+{
+	_tprintf(_T("\n"));
+	fclose(fDump);
+}
+*/
+/* Adust references to dllimported data that have non-zero offsets.  */
+//extern void _pei386_runtime_relocator (void);
+
+//extern "C"  void _pei386_runtime_relocator (void){};
+/*
+const class StaticClass
+{
+public:
+    StaticClass() {printf("StaticClass constructor\n");};
+    ~StaticClass() {printf("StaticClass destructor\n");};
+};
+StaticClass staticClass;
+*/
+
+/*
+int fTest(){
+	return 5;
+}
+int _nTest = fTest();
+
+//int _nTest = 5;
+ extern "C" int atexit(void (*function)(void)){
+	 
+ }
+
+
+ 
+extern "C" int entry()  //entry is the entry point instead of main
+{
+	int _nIn = 5;
+	
+	float _nFlaot = 6.5;
+	
+	_nIn =(int) _nFlaot;
+	
+	printf("\n--entry--");
+ // exit(0);
+ return 0;
+} 
+*/
+/*
+ extern "C" int atexit(void (*function)(void)){
+	 
+ }
+*/
+/*
+/*
+ void exit(int exit_code) {
+   system_dependent_exit(exit_code);
+}*/
+
+int _nTest = 5;
+	
+//__attribute__ ((visibility ("visible"))) 
+extern "C" int test()
+{
+	printf("\n--!test!-- %s\n");
+	return 0;
+}
+	
+
+	
+extern "C" int Main()
+{
+	printf("\n--!Main!--\n");
+	return 0;
+}
+	
+
+	
+	
+extern "C" int cpc_main(int argc, char **argv)
+{
+	printf("\n--aa cpc_main Test%s \n", argv);
+	printf("\n--aaTest%d \n", _nTest);
+	printf("\n--End-- \n");
+	return 0;
+}
+
+/*
+extern "C" int entry(int argc, char **argv)
+{
+	printf("\n--aaTest%d \n", _nTest);
+	printf("\n--End-- \n");
+	return 0;
+}
+*/
+/*
+extern "C" int __main(int argc, char **argv){
+		printf("\n--__main--");
+		return 0;
+}
+
+extern "C" int main(int argc, char **argv)
+{
+	printf("\n--Main \n");
+	cpc_main(argc, argv);
+
+	return 0;
+}*/
