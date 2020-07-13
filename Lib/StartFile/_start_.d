2e2f259983180f52ede3a0c2f476a0011fa55146
maeiky <maeiky@gmail.com>
2020-07-12 22:15:42 -0400

    Fix

--- Lib/StartFile/_start_.d
+++ Lib/StartFile/_start_.d
@@ -1,17 +1,17 @@
 StartFile/_start_.o: StartFileSrc\_start_.cpp \
-  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\CpcDosRT\CpcDosRT-0.5\SDK\stdio.h \
-  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\clang_inc\..\i686-w64-mingw32\include\stdio.h \
-  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\crtdefs.h \
-  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\_mingw.h \
-  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\_mingw_mac.h \
-  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\_mingw_secapi.h \
-  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\clang_inc\vadefs.h \
-  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\vadefs.h \
-  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\sdks\_mingw_directx.h \
-  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\sdks\_mingw_ddk.h \
-  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\_mingw_print_push.h \
-  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\_mingw_off_t.h \
-  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\swprintf.inl \
-  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\sec_api\stdio_s.h \
-  E:\Data\setting\Test\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-8.0.2\i686-w64-mingw32\include\_mingw_print_pop.h
-Cwc:VLianceTool/CpcDosRT Default clang++ (2019-03-18 14:14:22) >>  -O2 -target i686-pc-mingw32 -I"E:/Data/setting/Test/_Cwc/Toolchain/VLianceTool/LibRT/LibRT-8.0.2/clang_inc/" -I"E:/Data/setting/Test/_Cwc/Toolchain/VLianceTool/LibRT/LibRT-8.0.2/pthreads/" -I"E:/Data/setting/Test/_Cwc/Toolchain/VLianceTool/LibRT/LibRT-8.0.2/lib/gcc/i686-w64-mingw32/8.1.0/include/" -I"E:/Data/setting/Test/_Cwc/Toolchain/VLianceTool/CpcDosRT/CpcDosRT-0.5/Lib/" -fno-rtti -fno-exceptions -m32 -fcolor-diagnostics -fansi-escape-codes -Wno-pragma-pack -m32 -Wglobal-constructors -Wexit-time-destructors -Werror=return-type -Wno-return-type-c-linkage -MD -DD_Render_100_Frames -Wglobal-constructors -Wexit-time-destructors -Werror=global-constructors -Wexit-time-destructors -DD_Platform_Windows -DGZ_D_Monothread -I"E:/Data/setting/Test/_Cwc/Toolchain/VLianceTool/CpcDosRT/CpcDosRT-0.5/SDK/" -DDsToolchain="VLianceTool_CpcDosRT" -DDsPlatform="CpcDos" -DD_Platform_CpcDos -DDsPlatform_File="Default" -DD_Release -DDsOpt="O2"
+  E:\_Project\_Cwc\Toolchain\VLianceTool\CpcDosRT\CpcDosRT-999\SDK\stdio.h \
+  E:\_Project\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-999\clang_inc\..\i686-w64-mingw32\include\stdio.h \
+  E:\_Project\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-999\i686-w64-mingw32\include\crtdefs.h \
+  E:\_Project\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-999\i686-w64-mingw32\include\_mingw.h \
+  E:\_Project\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-999\i686-w64-mingw32\include\_mingw_mac.h \
+  E:\_Project\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-999\i686-w64-mingw32\include\_mingw_secapi.h \
+  E:\_Project\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-999\clang_inc\vadefs.h \
+  E:\_Project\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-999\i686-w64-mingw32\include\vadefs.h \
+  E:\_Project\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-999\i686-w64-mingw32\include\sdks\_mingw_directx.h \
+  E:\_Project\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-999\i686-w64-mingw32\include\sdks\_mingw_ddk.h \
+  E:\_Project\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-999\i686-w64-mingw32\include\_mingw_print_push.h \
+  E:\_Project\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-999\i686-w64-mingw32\include\_mingw_off_t.h \
+  E:\_Project\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-999\i686-w64-mingw32\include\swprintf.inl \
+  E:\_Project\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-999\i686-w64-mingw32\include\sec_api\stdio_s.h \
+  E:\_Project\_Cwc\Toolchain\VLianceTool\LibRT\LibRT-999\i686-w64-mingw32\include\_mingw_print_pop.h
+Cwc:VLianceTool/CpcDosRT Default clang++ (3/18/2019 2:14:22 PM) >>  -O2 -DNDEBUG -target i686-pc-mingw32 -I"E:/_Project/_Cwc/Toolchain/VLianceTool/LibRT/LibRT-999/clang_inc/" -I"E:/_Project/_Cwc/Toolchain/VLianceTool/LibRT/LibRT-999/pthreads/" -I"E:/_Project/_Cwc/Toolchain/VLianceTool/LibRT/LibRT-999/lib/gcc/i686-w64-mingw32/8.1.0/include/" -I"E:/_Project/_Cwc/Toolchain/VLianceTool/CpcDosRT/CpcDosRT-999/Lib/" -fno-rtti -fno-exceptions -m32 -fcolor-diagnostics -fansi-escape-codes -Wno-pragma-pack -m32 -Wglobal-constructors -Wexit-time-destructors -Werror=return-type -Wno-return-type-c-linkage -MD -DD_Render_100_Frames -Wglobal-constructors -Wexit-time-destructors -Werror=global-constructors -Wexit-time-destructors -DD_Platform_Windows -DGZ_D_Monothread -I"E:/_Project/_Cwc/Toolchain/VLianceTool/CpcDosRT/CpcDosRT-999/SDK/" -DDsToolchain="VLianceTool_CpcDosRT" -DDsPlatform="CpcDos" -DD_Platform_CpcDos -DDsPlatform_File="Default" -DD_Release -DDsOpt="O2"
--- Lib/StartFileSrc/_start_.cpp
+++ Lib/StartFileSrc/_start_.cpp
@@ -1,14 +1,15 @@
 #include <stdio.h>
-
-extern "C" int Main();
+extern "C" int Main(char*);
 
 //__attribute__ ((visibility ("visible"))) 
 //extern "C" int _start_(int argc, char **argv)
 extern "C" int _start_()
 {
-	//printf("\n--_start_-- %s\n", argv);
-	printf("\n--_start_-- \n");
-	return Main();
+	return Main(0);
 }
-
-
+//Required!? (Normally this function will never be called)
+extern "C" int __main(int argc, char **argv){
+	printf("\n--?!--\n");
+	return Main(0);
+}
+///
